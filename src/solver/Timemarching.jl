d668ad67f441a1f1de689dce960fe46fe80d0687
Yuxuan Li <liyuxuan48@live.com>
2025-11-08 00:51:37 -0500

    Update Timemarching.jl
    make the timemarching scheme more readable, make initial infochange before the simulation, and enable using two threads to speed up a little.

--- src/solver/Timemarching.jl
+++ src/solver/Timemarching.jl
@@ -2,13 +2,25 @@ import ConstrainedSystems: init, solve
 
 export ODE_innertube,ODE_steadyfilm,timemarching!
 
+"""
+    ODE_innertube(u,p,t)
+The ODE function for the tube system, to be used in `ODEProblem`.
+It takes the current state vector `u`, the parameter struct `p` (which is a `PHPSystem`), and the current time `t`.
+It returns the time derivative of the state vector `du`.
+`p` is used as the input for the dynamicsmodel and liquidmodel to get the current system state. Needs to be updated
+    everytime this function is called. When called by the ODE solver, `p` is not updated automatically when `u` is 
+    changed, this can be because of callbacks or substeps of the ODE solver.
+getcurrentsys_nowall! is used to update the system state from `u` and `p`, without updating wall temperature.
+"""
+
 function ODE_innertube(u,p,t)
 
     sys_init = p
 
     index_dynamics_end = findfirst(x->abs(x+1e10) <= 10^(-1), u)
 
-    newsys = getcurrentsys!(u,sys_init)
+    # These updated don't have wall temperature updated yet so getcurrentsys_nowall! can be used.
+    newsys = getcurrentsys_nowall!(u,sys_init)
 
     dynamicsdu = dynamicsmodel(u[1:index_dynamics_end-1],newsys)
 
@@ -24,7 +36,7 @@ end
 
 #     index_dynamics_end = findfirst(x->abs(x+1e10) <= 10^(-1), u)
 
-#     newsys = getcurrentsys!(u,p)
+#     newsys = getcurrentsys_nowall!(u,p)
 
 #     dynamicsdu = dynamicsmodel_steadyfilm(u[1:index_dynamics_end-1],newsys)
 
@@ -36,31 +48,43 @@ end
 
 # end
 
-function temperature_linesource(integrator_plate)
+"""
+    temperature_linesource(integrator_plate::ODEIntegrator)
+
+Get the temperature distribution on the plate from the plate integrator, to be used as wall temperature boundary condition for the tube system.
+"""
+
+function temperature_linesource(integrator_plate::ODEIntegrator)
+
     T = ComputationalHeatTransfer.temperature(integrator_plate)
     ohp_E = integrator_plate.p.extra_cache.fcache[end].region_cache.cache.E;
     ohp_E*T
 end
 
-# weakly coupled alternate time marching
-function timemarching!(integrator_tube,integrator_plate,tstep::Float64)
+"""
+    timemarching!(integrator_tubez::ODEIntegrator,integrator_plate::ODEIntegrator,tstep::Float64)
 
-    currentsys = getcurrentsys!(integrator_tube.u,integrator_tube.p)
-    currentsys.wall.θarray = temperature_linesource(integrator_plate)
+Perform one time step of size `tstep` for both the tube and plate integrators, exchanging information between them.
+This is a Jacobi style weakly coupled alternate time marching (exchange info at the same time after each step)
+"""
 
-    # println(currentsys.wall.θarray)
-    # currentsys.wall.θarray = temperature_linesource(integrator_plate) .+ Tstart
+function timemarching!(integrator_tube::ODEIntegrator,integrator_plate::ODEIntegrator,tstep::Float64)
 
-    # println(currentsys.wall.θarray)
-    qtmp = sys_to_heatflux(currentsys)
-    # println(qtmp)
- 
-    integrator_plate.p.phys_params["ohp_flux"] = -qtmp # q source per length
-    
-    step!(integrator_tube,tstep,true);
-    step!(integrator_plate,tstep,true);
-    # ADI_timemarching!(temperature(integrator_plate),sys_plate,tstep)
-    # integrator_plate.t += tstep
+    # initial exchange info at t=0
+    if isapprox(integrator_tube.t,0.0)
+        exchangepinfo!(integrator_tube,integrator_plate) # initial exchange info
+        println("Initial exchange info done.")
+    end
+
+    # use two threads to step the two integrators together
+    @sync begin
+         Threads.@spawn step!(integrator_tube,tstep,true);
+         Threads.@spawn step!(integrator_plate,tstep,true);
+    end
+
+    # exchange info after stepping at each time step
+    exchangepinfo!(integrator_tube,integrator_plate)
+
     integrator_tube,integrator_plate
 end
 
@@ -79,4 +103,18 @@ function init(u_tube::Vector{Float64},tspan::Tuple{Any, Any},sys_tube::PHPSystem
     prob = ODEProblem(ODE_innertube, u_tube, tspan, sys_tube,kwargs...) # construct integrator_tube problem
     return init(prob, alg=RK4(),dt=1e-3,save_on=false, callback=cbst,maxiters=1e10,kwargs...)
     # return init(prob, alg=RK4(),dt=1e-3,save_on=false, maxiters=1e10,kwargs...)
+end
+
+function exchangepinfo!(integrator_tube::ODEIntegrator,integrator_plate::ODEIntegrator)
+
+    # update the system of integrator_tube.p from the latest integrator_tube.u, without updating wall temperature
+    currentsys = getcurrentsys_nowall!(integrator_tube.u,integrator_tube.p)
+    # update interpolated wall temperature to tube system
+    currentsys.wall.θarray = temperature_linesource(integrator_plate)
+
+    # update heat flux to plate system
+    qtmp = sys_to_heatflux(currentsys)
+    integrator_plate.p.phys_params["ohp_flux"] = -qtmp # q' source per length
+    
+    integrator_tube,integrator_plate
 end
