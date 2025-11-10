c772c0af3b6835ceb12ce365202d595ed38c8a9d
Yuxuan Li <liyuxuan48@live.com>
2025-11-10 11:41:06 -0500

    Update liquidmerging.jl

--- src/solver/callback/liquidmerging.jl
+++ src/solver/callback/liquidmerging.jl
@@ -3,7 +3,7 @@ export merging_affect!,merging_condition,nucleateboiling,merging
 function merging_affect!(integrator)
 
     p = getcurrentsys_nowall(integrator.u,integrator.p);
-    δv = DEFAULT_MERGE_FRAC*p.wall.L_newbubble
+    δv = DEFAULT_LIQUID_MERGE_FRAC*p.wall.L_newbubble
 
     merge_flags = getmerge_flags(δv,p)
     indexmergingsite = sort(findall(x->x == true, merge_flags),rev = true)
@@ -39,7 +39,7 @@ function merging_condition(u,t,integrator)     # only for closed loop tube
 
     p = getcurrentsys_nowall!(integrator.u,integrator.p);
     # δv = p.tube.d > (integrator.dt*maximum(p.liquid.dXdt)[1]) ? p.tube.d : (integrator.dt*maximum(p.liquid.dXdt)[1])
-    δv = DEFAULT_MERGE_FRAC*p.wall.L_newbubble
+    δv = DEFAULT_LIQUID_MERGE_FRAC*p.wall.L_newbubble
 
     sys = p
 
